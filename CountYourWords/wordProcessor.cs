71a9a2b0b7c8cc11d33013da695a69329e88d55e
Deniz-ME <d.m.erdogan@student.vu.nl>
2025-04-12 17:14:46 +0200

    feat: added functions for different features within processing. ToLowerCase function now passes the test set up for it.

--- CountYourWords/wordProcessor.cs
+++ CountYourWords/wordProcessor.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("CountYourWordsTests")] //In this way only the test File is able to see the "private" functions.
 
 namespace CountYourWords
 {
@@ -8,8 +10,28 @@ namespace CountYourWords
     {
         public List<string> CleanText(string input)
         {
-            
-            
+            var lowercasedInput = ToLowercase(input);
+            var result = new List<string> {};
+            return result;
+        }
+
+        internal string ToLowercase(string input) 
+        {
+            return input.ToLower();
+        }
+        internal string RemoveNonAlphabeticChar(string input) 
+        {
+            return "";
+        }
+        internal List<string> SplitStringIntoList(string input) 
+        {
+            var result = new List<string> {};
+            return result;
+        }
+        internal List<string> RemoveEmptyWordsAndSpaces(List<string> input) 
+        {
+            var result = new List<string> {};
+            return result;
         }
     }
 }
--- CountYourWordsTests/obj/Debug/net8.0/countYourWordsTests.AssemblyInfo.cs
+++ CountYourWordsTests/obj/Debug/net8.0/countYourWordsTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("countYourWordsTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b9c0a45a2fb521ca5b4c91244439c092c51d5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6189e8c397c603a9bf1b641e28b8ea347893cdbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("countYourWordsTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("countYourWordsTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
