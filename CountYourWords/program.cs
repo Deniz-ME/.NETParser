535dcc59c93f95d646caaa50dbcbbb222281ac4b
Deniz-ME <d.m.erdogan@student.vu.nl>
2025-04-12 22:04:46 +0200

    feat: added the main, combining all the files and functions with eachother to have the final result. Which works accordingly!

--- CountYourWords/program.cs
+++ CountYourWords/program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace CountYourWords
 {
@@ -6,8 +8,29 @@ namespace CountYourWords
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello from CountYourWords!");
-    
+            string filePath = "CountYourWords/input.txt";
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("input.txt not found.");
+                return;
+            }
+
+            string textOutOfFile = File.ReadAllText(filePath);
+            
+            var processor = new WordProcessor();
+            var onlyWords = processor.CleanText(textOutOfFile);
+  
+            var counter = new WordCounter();
+            var wordCounts = counter.CountWords(onlyWords);
+
+            var sorter = new WordSorter();
+            var sortedWords = sorter.SortingAlphabetically(wordCounts);
+
+            Console.WriteLine($"\nNumber of words: {onlyWords.Count}\n");
+            foreach (var pair in sortedWords)
+            {
+                Console.WriteLine($"{pair.Key} {pair.Value}");
+            }
         }
     }
 }
